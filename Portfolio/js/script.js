40ebcee173f563225dc506125e8a35744bb4234f
MikaelArseneau <mikael.arseneau@hotmail.ca>
2025-10-04 15:34:34 -0400

    creation des liens pour les porjets

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -12,12 +12,12 @@ document.addEventListener('DOMContentLoaded', () => {
                     { id: 'troisDimension', name: '3D' }
                 ],
                 projects: [
-                    { id: 1, categories: 'web', title: 'Métrage', img: './media/metrage.png' },
-                    { id: 2, categories: 'web', title: 'DIX 10', img: './media/dix10.png' },
-                    { id: 3, categories: 'montage', title: 'Coeur de la montage', img: './media/coeur_de_la_montagne.jpg' },
-                    { id: 4, categories: 'montage', title: 'Terminus', img: './media/terminus.jpg' },
-                    { id: 5, categories: 'troisDimension', title: 'Chasseur de crane', img: './media/chasseur.png' },
-                    { id: 6, categories: 'montage', title: 'La Montée', img: './media/montee.png' }
+                    { id: 1, categories: 'web', title: 'Métrage', img: './media/metrage.png', dossier: 'metrage.html' },
+                    { id: 2, categories: 'web', title: 'DIX 10', dossier: "dix10.html", img: './media/dix10.png' },
+                    { id: 3, categories: 'montage', title: 'Coeur de la montage', dossier: "coeur.html", img: './media/coeur_de_la_montagne.jpg' },
+                    { id: 4, categories: 'montage', title: 'Terminus', img: './media/terminus.jpg', dossier: "terminus.html" },
+                    { id: 5, categories: 'troisDimension', title: 'Chasseur de crane', img: './media/chasseur.png', dossier: "chasseur.html" },
+                    { id: 6, categories: 'montage', title: 'La Montée', img: './media/montee.png', dossier: "montee.html" },
                 ]
             };
         },
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
             trigger: "#logiciel",
             start: "top 80%",
             toggleActions: "play none none none",
-            markers: true,
+
 
         }
     });
@@ -46,23 +46,6 @@ document.addEventListener('DOMContentLoaded', () => {
         { x: 0, opacity: 1, duration: 0.6, stagger: 0.3, ease: "power2.out" }
     );
 });
-tl.add(() => {
-    gsap.to("#logo-figma", {
-        scale: 1.1,
-        rotation: 360,
-        duration: 3,
-        repeat: -1,
-        yoyo: true,
-        ease: "power1.inOut"
-    });
 
-    gsap.to(".titre-logiciel", {
-        color: "#F24E1E",
-        duration: 1,
-        repeat: -1,
-        yoyo: true,
-        ease: "sine.inOut"
-    });
-});
 
 
